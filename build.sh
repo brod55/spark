c202a4915d289b54ba4ed4e92a09d3b6d1043342
root <root@tank>
2019-09-13 07:40:52 -0700

    initial commit

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t spark-base:2.4.4 ./docker/spark-base
-docker build -t spark-master:2.4.4 ./docker/spark-master
-docker build -t spark-worker:2.4.4 ./docker/spark-worker
-docker build -t spark-submit:2.4.4 ./docker/spark-submit
+docker build -t spark-base:2.4.4 ./spark-base
+docker build -t spark-master:2.4.4 ./spark-master
+docker build -t spark-worker:2.4.4 ./spark-worker
+docker build -t spark-submit:2.4.4 ./spark-submit
